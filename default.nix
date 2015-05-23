a95fe19273ee838f7d5f5b6b438162ccc90b7553
Matthias Beyer <mail@beyermatthias.de>
2015-05-23 17:59:19 +0200

    Rewrote default.nix to be a package definition

--- default.nix
+++ default.nix
@@ -1,49 +1,47 @@
-{ pkgs ? (import <nixpkgs> {})
-, haskellPackages ? pkgs.haskellPackages_ghc784 }:
+{ stdenv
+, pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellngPackages }:
 
-haskellPackages.cabal.mkDerivation (self: {
-    pname = "rate.hs";
+let
+  env = haskellPackages.ghcWithPackages (p: with p; [
+    pkgs.stdenv
+    haskellPackages.cabal-install
+    hlint
+
+    snap
+    snap-core
+    snap-server
+
+    snaplet-persistent
+
+    persistent
+    persistent-postgresql
+    persistent-sqlite
+
+    heist
+
+    # maybe later:
+
+    # rest-snap
+    # aeson
+
+    # snap-extras
+    # snaplet-hslogger
+  ]);
+in
+
+stdenv.mkDerivation rec {
+    name = "rate.hs";
+    src = ./src;
     version = "0.0.0.0";
 
-    src = ./.;
+    buildInputs = [ env ];
 
-    isLibrary = false;
-    isExecutable = true;
-
-    buildDepends = with pkgs.haskellngPackages; [
-      pkgs.stdenv
-      haskellPackages.cabalInstall
-      hlint
-
-      snap
-      snap-core
-      snap-server
-
-      snaplet-persistent
-
-      persistent
-      persistent-postgresql
-      persistent-sqlite
-
-      heist
-
-      # maybe later:
-
-      # rest-snap
-      # aeson
-
-      # snap-extras
-      # snaplet-hslogger
-    ];
-
-    testDepends = with haskellPackages; [ ];
-
-    meta = {
-      homepage = "http://github.com/hamot/hamot";
-      description = "Haskell Money Tracker application";
-      license = self.stdenv.lib.licenses.gpl2;
-      platforms = self.ghc.meta.platforms;
-    };
-})
-
+    # meta = {
+    #   homepage = "http://github.com/hamot/hamot";
+    #   description = "Haskell Money Tracker application";
+    #   license = self.stdenv.lib.licenses.gpl2;
+    #   platforms = self.ghc.meta.platforms;
+    # };
+}
 
