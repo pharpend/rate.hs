d0cc0552fa4f1c272420d93335200cc29b544377
Matthias Beyer <mail@beyermatthias.de>
2015-05-28 19:33:31 +0200

    Add FromRow instance (derived) for Entity

--- src/DB/Entity.hs
+++ src/DB/Entity.hs
@@ -2,7 +2,8 @@ module DB.Entity where
 
 import DB.Rate (Rate)
 import DB.Category (Category)
+import Database.PostgreSQL.Simple.FromRow
 
-data Entity = Category | Rate deriving (Show, Eq, Ord)
+data Entity = Category | Rate deriving (Show, Eq, Ord, FromRow)
 
 
