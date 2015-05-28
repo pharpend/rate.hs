7e0a01661359cd82b6fd4ee4d7d117fdca5d38d0
Matthias Beyer <mail@beyermatthias.de>
2015-05-28 20:28:10 +0200

    Remove Entity

--- src/DB/Entity.hs
+++
@@ -1,9 +0,0 @@
-module DB.Entity where
-
-import DB.Rate (Rate)
-import DB.Category (Category)
-import Database.PostgreSQL.Simple.FromRow
-
-data Entity = Category | Rate deriving (Show, Eq, Ord, FromRow)
-
-
