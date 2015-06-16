2409261d7de6807b6b652ddf5902325da7aba506
Matthias Beyer <mail@beyermatthias.de>
2015-06-16 20:26:36 +0200

    Remove everything which isnt plain heist and routing

--- src/Application.hs
+++ src/Application.hs
@@ -7,38 +7,23 @@
 module Application
 ( App(..)
 , heist
-, sess
-, auth
-, pg
 , AppHandler
 ) where
 
 ------------------------------------------------------------------------------
 import Control.Lens
 import Snap
-import Control.Monad.State
-import Control.Monad.Reader
 import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
-import Snap.Snaplet.Session
-import Snap.Snaplet.PostgresqlSimple
 
 ------------------------------------------------------------------------------
 data App = App
     { _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
-    , _pg    :: Snaplet Postgres
     }
 
 makeLenses ''App
 
 instance HasHeist App where
     heistLens = subSnaplet heist
-
-instance HasPostgres (Handler b App) where
-  getPostgresState = with pg get
-  setLocalPostgresState s = local $ set (pg . snapletValue) s
 
 ------------------------------------------------------------------------------
 type AppHandler = Handler App App
